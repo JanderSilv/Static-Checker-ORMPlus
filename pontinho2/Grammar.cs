98a17d1d40bce2dcc7abc4bd2a90d3c3c6784043
Ruannilton <ruannilton@gmail.com>
2021-06-02 02:38:49 -0300

    improv

--- pontinho2/Grammar.cs
+++ pontinho2/Grammar.cs
@@ -6,108 +6,146 @@ using OfficeOpenXml;
 
 public class Grammar
 {
-    public ReadOnlyCollection<Transicao> transitions;
-    public ReadOnlyCollection<int> finals;
-    public readonly string parsedString;
-    private string fileData;
+    public ReadOnlyDictionary<string, ReadOnlyCollection<Transicao>> transitions;
+    public ReadOnlyDictionary<string, ReadOnlyCollection<int>> finals;
+
+    private Dictionary<string, string> inputs = new();
+    private Dictionary<string, string> outputs = new();
+
+    private List<string> nTerminais = new();
 
     public Grammar(string filePath)
     {
-        fileData = File.ReadAllText(filePath).Replace('.', ' ').Replace('\n', ' ');
 
-        GramSource source = new(fileData);
-        Reader r = new(source, 0, 1);
+        Dictionary<string, ReadOnlyCollection<Transicao>> _transicoes = new();
+        Dictionary<string, ReadOnlyCollection<int>> _finais = new();
 
-        var t = r.Run();
-        var f = r.GetFinais();
-        parsedString = r.GetContent();
+        var file = new FileInfo(filePath);
+        var fileName = file.Name.Replace(file.Extension, "");
+        string fileData = File.ReadAllText(filePath).Replace('\n', ' ');
+        var skip = 0;
 
-        transitions = new ReadOnlyCollection<Transicao>(t);
-        finals = new ReadOnlyCollection<int>(f);
+        while (skip < fileData.Length)
+        {
+            string str = string.Concat(fileData.Skip(skip).TakeWhile(x => x != '.')).Trim();
+            string nTerminal = string.Concat(str.TakeWhile(x => x != '=')).Trim();
+            string data = string.Concat(str.Skip(nTerminal.Length + 1)).Trim();
+
+            if (string.IsNullOrWhiteSpace(str) || string.IsNullOrWhiteSpace(nTerminal) || string.IsNullOrWhiteSpace(data))
+            {
+                skip += str.Length + 1;
+                continue;
+            }
+
+            GramSource source = new(data);
+            Reader reader = new(source, 0, 1);
+
+
+            nTerminais.Add(nTerminal);
+            _transicoes[nTerminal] = new ReadOnlyCollection<Transicao>(reader.Run());
+            _finais[nTerminal] = new ReadOnlyCollection<int>(reader.GetFinais());
+            inputs[nTerminal] = data;
+            outputs[nTerminal] = ".0 " + reader.GetContent();
+            skip += str.Length + 1;
+        }
+        transitions = new(_transicoes);
+        finals = new(_finais);
+
+        Directory.CreateDirectory($"outputs/{fileName}");
+        SaveFile($"outputs/{fileName}/");
+        SaveTable($"outputs/{fileName}/");
 
     }
 
     public Grammar SaveFile(string folderPath)
     {
-        List<string> tofile = new(transitions.Count);
-        var atom = transitions.Where(x => x.entrada != "ε");
-        var empty = transitions.Where(x => x.entrada == "ε");
-
-        tofile.Add($"Entrada:\n{fileData}");
-        tofile.Add($"Saída:\n{parsedString}");
-        tofile.Add($"\nInício: 0");
-        tofile.Add($"Finais: {string.Join(',', finals)}\n");
-
-        tofile.Add("Transicoes:");
-        foreach (var item in atom)
+        foreach (var nt in nTerminais)
         {
-            tofile.Add(item.ToString());
+            List<string> tofile = new(transitions[nt].Count);
+            var atom = transitions[nt].Where(x => x.entrada != "ε");
+            var empty = transitions[nt].Where(x => x.entrada == "ε");
+
+            tofile.Add($"Entrada:\n{nt} = {inputs[nt]}.");
+            tofile.Add($"Saída:\n{nt} = {outputs[nt]}");
+            tofile.Add($"\nInício: 0");
+            tofile.Add($"Finais: {string.Join(',', finals[nt])}\n");
+
+            tofile.Add("Transicoes:");
+            foreach (var item in atom)
+            {
+                tofile.Add(item.ToString());
+            }
+
+            foreach (var item in empty)
+            {
+                tofile.Add(item.ToString());
+            }
+
+            File.WriteAllLines(folderPath + $"{nt}.txt", tofile);
         }
 
-        foreach (var item in empty)
-        {
-            tofile.Add(item.ToString());
-        }
-
-        File.WriteAllLines(folderPath, tofile);
         return this;
     }
 
     public Grammar SaveTable(string folderPath)
     {
-        ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-        var file = new FileInfo(folderPath);
-        if (file.Exists)
+        foreach (var nt in nTerminais)
         {
-            file.Delete();
-        }
-
-        using var package = new ExcelPackage(file);
-        var worksheet = package.Workbook.Worksheets.Add("Notacao Tabular");
-
-
-        var groups = transitions.GroupBy(x => x.entrada);
-        int qntStates = transitions.Max(x => x.proximo) + 1;
-
-
-
-        for (int i = 0; i < qntStates; i++)
-        {
-            worksheet.Cells[i + 2, 1].Value = i;
-        }
-
-        int collumn = 2;
-        IGrouping<string, Transicao> vazio = null;
-        foreach (var g in groups)
-        {
-            string key = g.Key;
-            if (key == "ε")
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            var file = new FileInfo(folderPath + $"{nt}.xlsx");
+            if (file.Exists)
             {
-                vazio = g;
-                continue;
+                file.Delete();
             }
 
-            worksheet.Cells[1, collumn].Value = key;
-            var tr = g.OrderBy(x => x.estado).ToArray();
+            using var package = new ExcelPackage(file);
+            var worksheet = package.Workbook.Worksheets.Add(nt);
 
 
-            foreach (var t in tr)
+            var groups = transitions[nt].GroupBy(x => x.entrada);
+            int qntStates = transitions[nt].Max(x => x.proximo) + 1;
+
+
+
+            for (int i = 0; i < qntStates; i++)
+            {
+                worksheet.Cells[i + 2, 1].Value = i;
+            }
+
+            int collumn = 2;
+            IGrouping<string, Transicao> vazio = null;
+            foreach (var g in groups)
+            {
+                string key = g.Key;
+                if (key == "ε")
+                {
+                    vazio = g;
+                    continue;
+                }
+
+                worksheet.Cells[1, collumn].Value = key;
+                var tr = g.OrderBy(x => x.estado).ToArray();
+
+
+                foreach (var t in tr)
+                {
+                    worksheet.Cells[t.estado + 2, collumn].Value = t.proximo;
+
+                }
+                collumn++;
+            }
+
+            worksheet.Cells[1, collumn].Value = "ε";
+
+            foreach (var t in vazio.OrderBy(x => x.estado).ToArray())
             {
                 worksheet.Cells[t.estado + 2, collumn].Value = t.proximo;
 
             }
-            collumn++;
+
+            package.SaveAs(file);
         }
 
-        worksheet.Cells[1, collumn].Value = "ε";
-
-        foreach (var t in vazio.OrderBy(x => x.estado).ToArray())
-        {
-            worksheet.Cells[t.estado + 2, collumn].Value = t.proximo;
-
-        }
-
-        package.SaveAs(file);
         return this;
     }
 }
--- pontinho2/Program.cs
+++ pontinho2/Program.cs
@@ -3,11 +3,6 @@
 
 foreach (var path in Directory.GetFiles("inputs"))
 {
-    var f = new FileInfo(path);
-    var n = f.Name.Replace(f.Extension, "");
-    Directory.CreateDirectory($"outputs/{n}");
-
-    new Grammar(path).SaveFile($"outputs/{n}/{n}.txt").SaveTable($"outputs/{n}/{n}.xlsx");
-
+    new Grammar(path);
 }
 
